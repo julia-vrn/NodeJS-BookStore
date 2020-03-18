1f094995229595ab156f08cba375489545215d6d
julia-vrn <julia.voronetskaja@gmail.com>
2020-03-18 13:38:33 +0200

    Refactor product.js File

--- models/product.js
+++ models/product.js
@@ -1,49 +1,49 @@
 //working with files
 //fs - file system;
 
+//IMPORTANT! In regular functions the 'this' keyword represented the object that called the function, which could be the window, the document, a button or whatever.
+//IMPORTANT! With arrow functions the this keyword always represents the object that defined the arrow function.
+//https://www.w3schools.com/js/js_arrow_function.asp
+
 const fs = require('fs');
 const path = require('path');
 
+//to avoid repetition of the same chunk of code, dataPath is now a golbal variable
+const dataPath = path.join(path.dirname(process.mainModule.filename),
+    'data',
+    'product.json'        
+    );
+
+//code refactoring 
+//let's create a helper function to read the content of the file
+const getProductsFromFile = (callBack) => {
+    fs.readFile(dataPath, (error, fileContent) => {
+        //in case of  error while opening the file
+        if(error){
+            return callBack([]);
+        }
+        callBack(JSON.parse(fileContent));
+    });
+}
+
+//Model itself
 module.exports = class Product {
     constructor(bookTitle){
         this.title = bookTitle;
     }
 
     saveProduct() {
-        //path to the product.json file
-        const dataPath = path.join(path.dirname(process.mainModule.filename),
-        'data',
-        'product.json'        
-        );
-        fs.readFile(dataPath, (error, fileContent) => {
-            let products = [];
-            if(!error){
-                products = JSON.parse(fileContent);
-            }
+        getProductsFromFile(products => {
             products.push(this);
-
             //saving products array in json format to the file
             fs.writeFile(dataPath, JSON.stringify(products), (error) => {
                 console.log(error);
             });
-
         });
-        //before we save data to the file, we need to read it first
-
+       
     }
     //to read the products.json file content
     static fetchAllProducts(callBack){
-        const dataPath = path.join(path.dirname(process.mainModule.filename),
-        'data',
-        'product.json'        
-        );
-
-        fs.readFile(dataPath, (error, fileContent) => {
-            //in case of  error while opening the file
-            if(error){
-                return callBack([]);
-            }
-            callBack(JSON.parse(fileContent));
-        });
+        getProductsFromFile(callBack);
     }
 }
--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.use('/admin', adminRoute);
 app.use(errorPage.getErrorPage);
 
 
-app.listen(3000, ()=> {
+app.listen(5000, ()=> {
     console.log("Server is running on Port 3000.");
 })
